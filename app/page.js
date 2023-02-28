6650f9b6f66b120de3571cd95e8a5bb3ca089fd8
edeliscoding <edelreichd@gmail.com>
2023-02-27 20:45:02 -0700

    added Head

--- app/page.js
+++ app/page.js
@@ -1,18 +1,36 @@
+import Head from "next/head";
 import About from "./components/About";
 import Brands from "./components/Brands";
 import Contact from "./components/Contact";
 import HeroSection from "./components/HeroSection";
 import Testimonial from "./components/Testimonial";
-import { Roboto } from "@next/font/google";
 
 export default function Home() {
   return (
-    <main>
-      <HeroSection />
-      {/* <Brands /> */}
-      <About />
-      <Testimonial />
-      <Contact />
-    </main>
+    <>
+      <Head>
+        <title>
+          Personal Trainer, Athletic Performance Coach, and Fitness Consultant
+          in Aurora, CO | David Wood
+        </title>
+        <meta charset="UTF-8" />
+        <meta
+          name="description"
+          content="Looking for a personal trainer or athletic performance coach in Aurora, CO? Look no further! {Your Name} is an experienced coach and consultant who can help you reach your fitness goals."
+        />
+        <meta
+          name="keywords"
+          content="personal trainer, athletic performance coach, fitness consultant, Aurora CO, Santa Monica CA, UCLA, women's basketball, volleyball"
+        />
+        <meta name="author" content="David Wood" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <main>
+        <HeroSection />
+        <About />
+        <Testimonial />
+        <Contact />
+      </main>
+    </>
   );
 }
