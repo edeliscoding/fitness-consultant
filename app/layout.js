faeb34fb6bf7b0ee5dca21435032bd7110244edc
edeliscoding <edelreichd@gmail.com>
2023-02-27 22:40:05 -0700

    added metadata for SEO

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar";
 import "./globals.css";
 
+import Head from "next/head";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- app/page.js
+++ app/page.js
@@ -1,36 +1,26 @@
-import Head from "next/head";
 import About from "./components/About";
 import Brands from "./components/Brands";
 import Contact from "./components/Contact";
 import HeroSection from "./components/HeroSection";
 import Testimonial from "./components/Testimonial";
+import { Metadata } from "next";
+
+export const metadata = {
+  title: `Personal Trainer, Athletic Performance Coach, and Fitness Consultant
+  in Aurora, CO | David Wood`,
+  description: `Looking for a personal trainer or athletic performance coach in Aurora, CO? Look no further! David Wood is an experienced coach and consultant who can help you reach your fitness goals.`,
+  keywords: `personal trainer, athletic performance coach, fitness consultant, Aurora CO, Santa Monica CA, UCLA, women's basketball, volleyball`,
+  author: `David Wood`,
+  viewport: `width=device-width, initial-scale=1.0`,
+};
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>
-          Personal Trainer, Athletic Performance Coach, and Fitness Consultant
-          in Aurora, CO | David Wood
-        </title>
-        <meta charset="UTF-8" />
-        <meta
-          name="description"
-          content="Looking for a personal trainer or athletic performance coach in Aurora, CO? Look no further! {Your Name} is an experienced coach and consultant who can help you reach your fitness goals."
-        />
-        <meta
-          name="keywords"
-          content="personal trainer, athletic performance coach, fitness consultant, Aurora CO, Santa Monica CA, UCLA, women's basketball, volleyball"
-        />
-        <meta name="author" content="David Wood" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <main>
-        <HeroSection />
-        <About />
-        <Testimonial />
-        <Contact />
-      </main>
-    </>
+    <main>
+      <HeroSection />
+      <About />
+      <Testimonial />
+      <Contact />
+    </main>
   );
 }
